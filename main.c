44244ea304bfa27d099ccc16c068031f4635de97
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-08-25 19:31:14 -0700

    0-task commit

--- main.c
+++ main.c
@@ -8,34 +8,31 @@
  */
 int main(int ac __attribute__((unused)), char *av[], char *envp[])
 {
-char *buffer = NULL;
-size_t n = 0;
-int status;
-pid_t child_pid;
+char *b = NULL;
+size_t n;
+int s;
+pid_t c;
 signal(SIGINT, SIG_IGN);
 while (1)
 {
 if (isatty(STDIN_FILENO))
 printf("Simple_Shell$ ");
-if (getline(&buffer, &n, stdin) == -1)
+if (getline(&b, &n, stdin) == -1 || b == NULL)
 break;
-if (buffer == NULL)
-exit(0);
-av = parse_input(buffer);
+av = parse_input(b);
 if (!av[0])
 {
 free(av);
 continue;
 }
-if (_strcmp(av[0], "env") == 0)
-{
+if (strcmp(av[0], "env") == 0)
 print_environment(), free(av);
-continue;
-}
-if (_strcmp(av[0], "exit") == 0)
-free(av), free(buffer), exit(0);
-child_pid = fork();
-if (child_pid == 0)
+else if (strcmp(av[0], "exit") == 0)
+free(av), free(b), exit(0);
+else
+{
+c = fork();
+if (c == 0)
 {
 if (_strchr(av[0], '/') == NULL)
 av[0] = path_search(av[0]);
@@ -45,8 +42,9 @@ perror("execve"), exit(EXIT_FAILURE);
 break;
 }
 }
-wait(&status), free(av);
+wait(&s), free(av);
 }
-free(buffer);
+}
+free(b);
 return (0);
 }
